6b8a5e8da21ce9998c5b9d7b4a5d6cdd39b41a49
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-18 12:35:35 -0400

    Create _test.js

---
+++ plugins/_test.js
@@ -0,0 +1,15 @@
+
+//let fs = require('fs')
+
+let handler = async (m, { conn }) => {
+	
+	//let vidg = fs.readFileSync(`./src/mp4/1.mp4`)
+	let vidg = './src/mp4/1.mp4'
+	conn.sendFile(m.chat, vidg, 'ldn.gif', '', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
+	m.reply(`_La leyenda dice que si le das_ *doble click* _al gif escucharas la voz del tipo en tu mente._`)
+	}
+	 
+handler.customPrefix = /^linda noche?$/i
+handler.command = new RegExp
+
+export default handler
