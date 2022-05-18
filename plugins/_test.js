e570eb02bc303c1e54948221b62dc1cf557d5560
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-18 12:39:57 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,10 +1,10 @@
 
 //let fs = require('fs')
-
+import { fs } from 'fs'
 let handler = async (m, { conn }) => {
 	
-	//let vidg = fs.readFileSync(`./src/mp4/1.mp4`)
-	let vidg = './src/mp4/1.mp4'
+     let vidg = fs.readFileSync(`./src/mp4/1.mp4`)
+	//let vidg = './src/mp4/1.mp4'
 	conn.sendFile(m.chat, vidg, 'ldn.gif', '', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
 	m.reply(`_La leyenda dice que si le das_ *doble click* _al gif escucharas la voz del tipo en tu mente._`)
 	}
