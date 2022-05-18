d2b93ffbe07c353950cb1d0b6bf8b110436e84cb
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-18 12:47:40 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,15 +1,18 @@
+import { webp2mp4 } from '../lib/webp2mp4'
 
-//let fs = require('fs')
-import fs from 'fs'
-let handler = async (m, { conn }) => {
-	
-     let vidg = fs.readFileSync(`./src/mp4/1.mp4`)
-	//let vidg = './src/mp4/1.mp4'
-	conn.sendFile(m.chat, vidg, 'ldn.gif', '', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
-	m.reply(`_La leyenda dice que si le das_ *doble click* _al gif escucharas la voz del tipo en tu mente._`)
-	}
-	 
-handler.customPrefix = /^linda noche?$/i
-handler.command = new RegExp
+let handler = async (m, { conn, usedPrefix, command }) => {
+    if (!m.quoted) throw `✳️ Responde a un sticker con :\n\n*${usedPrefix + command}*`
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw `✳️ Responde a un sticker con :\n\n*${usedPrefix + command}*`
+    let media = await m.quoted.download()
+    let out = Buffer.alloc(0)
+    if (/webp/.test(mime)) {
+        out = await webp2mp4(media)
+    }
+    await conn.sendFile(m.chat, out, 'out.gif', '', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['togif <sticker>']
+handler.tags = ['sticker']
+handler.command = ['togif']
 
 export default handler
