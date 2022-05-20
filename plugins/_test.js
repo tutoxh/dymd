5baf6bd2f63caa1f275d37d0d6cf7039ab92b61f
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-20 11:18:12 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -3,11 +3,11 @@ import { tiktok } from '../lib/scrape'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	
-  if (!text) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
+  if (!text) throw `uhm.. url nya mana?\n\`
   if (!args[0].match(/tiktok/gi)) throw `link incorrecto`
    await m.reply(wait)
   
-  try {
+  //try {
   var anu = await Tiktok(args[0])
   var { url, title, thumbnail, duration, source, medias } = anu
   var { quality, extension, size, formattedSize, } = anu.medias[0]
@@ -19,18 +19,21 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 🗃 *Extension:* ${extension}
 🎚 *Size:* ${formattedSize}
 `
-  //conn.sendBV(m.chat, cap, global.wm, medias[0].url, [[`No WM`, `.tiktoknowm ${args[0]}`], [`Audio`, `.tiktokaudio ${args[0]}`]], null, {mentions: [m.sender]})
+  
  conn.sendFile(m.chat, medias[0].url, 'tk.mp4', sel, m) 
-  } catch {
+ /*} catch {
     try {
     var anuu = await tiktok(args[0])
     var { nowm, wm, audio } = anuu
-    //conn.sendBV(m.chat, cap, global.wm, wm, [[`No WM`, `.tiktoknowm ${args[0]}`], [`Audio`, `.tiktokaudio ${args[0]}`]], 0, {mentions: [m.sender]})
+
     conn.sendFile(m.chat, wm, 'tk.mp4', `✅ Aquí `, m) 
   } catch {
     throw `❎ Error` 
    }
- }
+   
+ }*/
+ 
+ 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
