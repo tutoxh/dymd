92c24269ecd37028a1863090c96437b7618dbca8
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-20 10:09:24 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -4,24 +4,27 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n*${usedPrefix + command}* https://fb.watch/aYv0jAffAO/`
-  if (!args[0].match(/(https:\/\/.www.facebook.com || fb.watch)/gi)) throw `*Link salah! Perintah ini untuk mengunduh media facebook dengan link*\n\ncontoh:\n${usedPrefix + command} https://fb.watch/aYv0jAffAO/`
+  if (!args[0]) throw `✳️ Ingrese un link de Facebook\n\n📌 Ejemplo :\n*${usedPrefix + command}* https://fb.watch/d7m6-k23cR/`
+  if (!args[0].match(/(https:\/\/.www.facebook.com || fb.watch)/gi)) throw `❎ *Link incorrecto*`
   await m.reply(wait)
   
       let b = await Facebook(args[0])
      let { title, thumbnail, duration, source, medias } = b
      let { url, quality, extension, size, formattedSize  } = medias[0]
      let urlshort = await(await axios.get(`https://tinyurl.com/api-create.php?url=${url}`)).data
-     let sell = `🎬 *Facebook MP4*\n\n✨ *Quality:* ${quality}\n🎚 *Size:* ${formattedSize}\n🚀 *Link:* ${urlshort}`
-  //   conn.sendMedia(m.chat, url, null, {caption: sell, mentions: [m.sender], jpegThumbnail: await(await fetch(thumbnail)).buffer()})
-     conn.sendFile(m.chat, url, 'fb.mp4', sell, m)
+     let fbt = `
+┌─⊷ *Facebook MP4*
+▢ *🎥 Calidad:* ${quality}
+▢ *🎚 Tamaño:* ${formattedSize}
+▢ *🔗 Link:* ${urlshort}
+└──────────`
+    conn.sendFile(m.chat, url, 'fb.mp4', fbt, m)
       
 }
-handler.help = ['facebook'].map(v => v + ' <url>')
+handler.help = ['facebook <url>']
 handler.tags = ['downloader']
-handler.command = /^((fb|facebook)(d(own)?l(oad)?(er)?)?(mp4)?)$/i
+handler.command = ['fb', 'facebook', 'fbdl'] 
 
 handler.limit = true
-
 
 export default handler
