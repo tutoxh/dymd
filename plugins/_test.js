9caef73f22adda09f764275367426d1973760eab
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-20 09:47:02 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,28 +1,26 @@
-import { webp2mp4 } from '../lib/webp2mp4.js'
-import { ffmpeg } from '../lib/converter.js'
 
-let handler = async (m, { conn }) => {
-    if (!m.quoted) throw '✳️ Responde a un sticker animado'
-    let mime = m.quoted.mimetype || ''
-    if (!/webp|audio/.test(mime)) throw '✳️ Responde a un sticker animado'
-    let media = await m.quoted.download()
-    let out = Buffer.alloc(0)
-    if (/webp/.test(mime)) {
-        out = await webp2mp4(media)
-    } else if (/audio/.test(mime)) {
-        out = await ffmpeg(media, [
-            '-filter_complex', 'color',
-            '-pix_fmt', 'yuv420p',
-            '-crf', '51',
-            '-c:a', 'copy',
-            '-shortest'
-        ], 'mp3', 'mp4')
-    }
-    await conn.sendFile(m.chat, out, 'tovid.mp4', '✅ hecho' , m)
+import { Facebook } from 'xfarr-api'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n*${usedPrefix + command}* https://fb.watch/aYv0jAffAO/`
+  if (!args[0].match(/(https:\/\/.www.facebook.com || fb.watch)/gi)) throw `*Link salah! Perintah ini untuk mengunduh media facebook dengan link*\n\ncontoh:\n${usedPrefix + command} https://fb.watch/aYv0jAffAO/`
+  await m.reply(wait)
+  
+      let b = await Facebook(args[0])
+     let { title, thumbnail, duration, source, medias } = b
+     let { url, quality, extension, size, formattedSize  } = medias[0]
+     let urlshort = await(await axios.get(`https://tinyurl.com/api-create.php?url=${url}`)).data
+     let sell = `🎬 *Facebook MP4*\n\n✨ *Quality:* ${quality}\n🎚 *Size:* ${formattedSize}\n🚀 *Link:* ${urlshort}`
+     conn.sendMedia(m.chat, url, null, {caption: sell, mentions: [m.sender], jpegThumbnail: await(await fetch(thumbnail)).buffer()})
+
+      
 }
-handler.help = ['tovid']
-handler.tags = ['sticker']
-handler.command = ['tovideo', 'tovid']
+handler.help = ['facebook'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^((fb|facebook)(d(own)?l(oad)?(er)?)?(mp4)?)$/i
+
+handler.limit = true
 
 
 export default handler
