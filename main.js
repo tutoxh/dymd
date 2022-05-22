71595c53330b5dbe128052278170f061d7f630dd
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-22 09:34:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ const { state, saveState } = store.useSingleFileAuthState(global.authFile)
 const connectionOptions = {
   printQRInTerminal: true,
   auth: state,
-  browser: ['dylux-fg','Brave','1.0.0'],
+  browser: ['dylux-fg','Safari','5.1.7'],
   // logger: pino({ level: 'trace' })
 }
 
