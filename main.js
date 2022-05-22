d9ae2c410a14883ae9776d72a175804a0007f6b6
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-22 09:42:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import './config.js';
-
+  
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 import path, { join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
