661d779779f3b8e8ea1241ad30d2ea797af95c9f
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-22 10:28:37 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,16 +1,19 @@
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
+    avatar: await conn.getProfilePicture(who).catch(_ => 'https://i.ibb.co/PZNv21q/Profile-FG98.jpg'),
+  })
+  let stiker = await sticker(false, marah, global.packname, global.author)
+  if (stiker) return await conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+  
+  throw stiker.toString()
+}
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!m.quoted) throw '✳️ Responde a un mensaje'
-    if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
-    let sticker = db.data.sticker
-    let hash = m.quoted.fileSha256.toString('hex')
-    if (!(hash in sticker)) throw 'No está registrado en la base de datos'
-    sticker[hash].locked = !/^un/i.test(command)
-    m.reply('✅ Hecho')
-} 
-handler.help = ['un', ''].map(v => v + 'lockcmd')
-handler.tags = ['database']
-handler.command = ['unlockcmd', 'lockcmd'] 
-handler.rowner = true
 
-export default handler
+handler.help = ['trigger']
+handler.tags = ['sticker']
+handler.command = ['trigger', 'triggered', 'ger'] 
+
+module.exports = handler
