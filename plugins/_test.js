dac22eceac846c417a048f10eb493eac624803eb
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-05-22 10:30:25 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,11 +1,12 @@
-const { sticker } = require('../lib/sticker')
-const { MessageType } = require('@adiwajshing/baileys')
+import { sticker } from '../lib/sticker.js'
+
 let handler = async (m, { conn }) => {
+	
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
-    avatar: await conn.getProfilePicture(who).catch(_ => 'https://i.ibb.co/PZNv21q/Profile-FG98.jpg'),
+    avatar: await conn.profilePictureUrl(who).catch(_ => 'https://i.ibb.co/PZNv21q/Profile-FG98.jpg'),
   })
-  let stiker = await sticker(false, marah, global.packname, global.author)
+let stiker = await sticker(false, marah, global.packname, global.author)
   if (stiker) return await conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
   
   throw stiker.toString()
@@ -16,4 +17,4 @@ handler.help = ['trigger']
 handler.tags = ['sticker']
 handler.command = ['trigger', 'triggered', 'ger'] 
 
-module.exports = handler
+export default handler
