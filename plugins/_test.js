247b3ffc15051fb5d72c531bea00a4f2a6ed9b4c
tutoxh <103659233+tutoxh@users.noreply.github.com>
2022-06-05 14:39:31 -0400

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
     let { premium, level, limit, exp, lastclaim, registered, regTime, age } = global.db.data.users[m.sender]
     let username = conn.getName(who)
     let name = conn.getName(who)
-    let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+   
     let str = `
 ]──────────❏ *PROFILE* ❏──────────[
 💌 • *Name:* ${username} 
@@ -23,7 +23,7 @@ ${readMore}
 ${clockString(user.premiumTime)}
 📑 • *Registered:* ${registered ? '✅': '❌'}
 `.trim()
-    conn.sendButton(m.chat, str, botdate, pp, [[`${registered ? 'Menu':'Verify'}`, `${user.registered ? '.menu':'.verify'}`]], fkon, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
+    conn.sendButton(m.chat, str, igfg, pp, [['👍🏻', ' '], ['🖤', ' ']], m)
 }
 handler.help = ['profile [@user]']
 handler.tags = ['exp']
